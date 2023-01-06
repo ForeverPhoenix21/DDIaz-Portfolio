f6ed40ea24842b5f4640dd5ea29f69621a4ff73f
ForeverPhoenix21 <Derek.Diaz86@gmail.com>
2023-01-06 16:52:01 -0500

    Fixed: Grid margins to center evenly by 10%  for left and right landingPage containers.

--- ddiaz-portfolio/src/components/LandingPage/LandingPage.styles.js
+++ ddiaz-portfolio/src/components/LandingPage/LandingPage.styles.js
@@ -7,25 +7,31 @@ export const LandingText = styled.p``;
 export const LandingContainer = styled.div`
   border: 1px solid red;
   display: grid;
-  grid-template-columns: repeat(10,10%);
+  grid-template-columns: repeat(10, 10%);
   grid-template-rows: repeat(10, 10%);
   height: 800px;
+  background-color: red;
 `;
 
 // The left Landing page container
 export const AsideLeft = styled.div`
-  
   border: 1px dotted red;
-  grid-column: 3/ 6;
-  height: 400px;
+  grid-column: 2/ 6;
+  grid-row: 3 /9;
+  /* height: 400px; */
+  width: 90%;
   text-align: center;
+  background-color: white;
 `;
 
 // The Right Landing Page container
 
 export const AsideRight = styled.div`
   border: 1px solid red;
-  grid-column: 6 /9;
-  height: 400px;
+  grid-column: 6 /10;
+  grid-row: 3 /9;
+  /* height: 400px; */
+  margin-left: 10%;
   text-align: center;
+  background-color: white;
 `;
