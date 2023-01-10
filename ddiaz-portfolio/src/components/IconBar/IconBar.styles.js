f597af22b51a904db9c4c572e036526416d3545d
ForeverPhoenix21 <Derek.Diaz86@gmail.com>
2023-01-10 12:02:08 -0500

    Added: IconContainer and IconTile to index.js and IconBar.styles.js

--- ddiaz-portfolio/src/components/IconBar/IconBar.styles.js
+++ ddiaz-portfolio/src/components/IconBar/IconBar.styles.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
+export const IconText = styled.p`
+margin-right: 40px;
+`
+
+export const IconBarContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height :60px;
+`
+
+export const IconTile = styled.div`
+margin: 0px 20px;
+`
+
+export const Icon = styled.img`
+`
 
--- ddiaz-portfolio/src/components/IconBar/index.js
+++ ddiaz-portfolio/src/components/IconBar/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
-import {} from "./IconBar.styles"
+import {Icon, IconBarContainer,IconTile,IconText} from "./IconBar.styles"
 
 const IconBar = () => {
   return (
-    <div><p>This is the IconBar</p></div>
+    <IconBarContainer>
+        <IconText>
+            Experience With:
+        </IconText>
+        <IconTile>
+            <Icon alt="HTML LOGO"/>
+        </IconTile>
+        <IconTile>
+            <Icon alt="CSS LOGO"/>
+        </IconTile>
+        <IconTile>
+            <Icon alt="JS LOGO"/>
+        </IconTile>
+        <IconTile>
+            <Icon alt="REACT LOGO"/>
+        </IconTile>
+        <IconTile>
+            <Icon alt="NodeJS LOGO"/>
+        </IconTile>
+        <IconTile>
+            <Icon alt="MongoDB LOGO"/>
+        </IconTile>
+    </IconBarContainer>
   )
 }
 
