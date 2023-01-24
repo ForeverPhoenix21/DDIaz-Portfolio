c78a8be053fd952ab72f55a59c66a0c93952557c
ForeverPhoenix21 <Derek.Diaz86@gmail.com>
2023-01-24 13:55:30 -0500

    ADDED: box-shadow to IconBarContainer in IconBar.styles.js

--- ddiaz-portfolio/src/components/IconBar/IconBar.styles.js
+++ ddiaz-portfolio/src/components/IconBar/IconBar.styles.js
@@ -13,9 +13,11 @@ export const IconBarContainer = styled.div`
   align-items: center;
   background-color: rgba(4, 25, 39, 0.05);
   height: 60px;
-  /* margin-left: 50px; */
+
   margin-left: 5%;
   margin-right: 5%;
+
+  box-shadow: .1em 0 .9em rgba(4, 25, 39, .5);
 `;
 
 export const IconTile = styled.div`
